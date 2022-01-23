b1785ab8758b2a6c8a1a56e814a3d455e12ff720
Leo Dao <leotomdao@gmail.com>
2022-01-23 14:31:10 +0100

    Create AdCardList component

---
+++ frontend/src/components/AdCardList/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import AdCard, { AdCardProps } from "../AdCard";
+import { List, Card, Row, Col } from 'antd';
+import Item from "antd/lib/list/Item";
+
+
+interface Props {
+    data: AdCardProps[],
+}
+
+/* const AdCardList: React.FunctionComponent<Props> = (props) => {
+
+
+    const listElements = props.data.map((value) => {
+        return (
+            <Col span={6} style={{ height: 50 }}>
+                <AdCard
+                    adImage={value.adImage}
+                    title={value.title}
+                    description={value.description}
+                    user={value.user}
+                />
+            </Col>)
+    })
+
+    return (
+        <Row gutter={16} >
+            {listElements}
+        </Row>
+    )
+} */
+
+const AdCardList: React.FunctionComponent<Props> = (props) => {
+    return (
+        <List
+            grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 4,
+                lg: 4,
+                xl: 6,
+            }}
+            dataSource={props.data}
+            renderItem={item => (
+                <List.Item>
+                    <AdCard
+                        adImage={item.adImage}
+                        title={item.title}
+                        description={item.description}
+                        user={item.user}
+                    />
+                </List.Item>
+            )}
+        />
+    )
+}
+
+export default AdCardList;
