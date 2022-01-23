60e3c4df35cea45c040aa132fff823241686c9a5
Leo Dao <leotomdao@gmail.com>
2022-01-23 14:31:19 +0100

    Create Header component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+import { CodeSandboxSquareFilled } from '@ant-design/icons';
+import { PageHeader, Button, } from 'antd';
+import React from 'react';
+
+interface Props {
+
+}
+
+
+
+const Header: React.FunctionComponent<Props> = (props) => {
+
+    const logIn = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        console.log(e);
+    };
+
+    return (
+        <PageHeader
+            style={{
+                border: '1px solid rgb(235, 237, 240)',
+            }}
+            className="site-page-header"
+            onBack={() => null}
+            title="AirGnB"
+            subTitle="Like AirBnB but not really"
+
+            extra={
+                [
+                    <Button key="3">Operation</Button>,
+                    <Button key="2">Operation</Button>,
+                    <Button onClick={logIn} key="1" type="primary">
+                        Log In
+                    </Button>
+                ]
+            }
+        ></PageHeader>
+    )
+}
+
+export default Header;
