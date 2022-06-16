86b8f08db094017f0efd3bc037461529ac4157cd
Leo Dao <leotomdao@gmail.com>
2022-06-16 16:27:56 +0200

    Added body-parser, server logs register info

--- root/backend/server.js
+++ root/backend/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const bp = require('body-parser');
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
+app.post('/register', (req, res) => {
+    // print the form data
+    console.log(req.body);
+}
+);
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
-    console.log("more  stuff as well")
 }
 );
