746669f1b97c55e7c5c2137e50bb0055b17b9bcb
Leo Dao <leotomdao@gmail.com>
2022-06-17 17:17:23 +0200

    Created routes folder, imported mongoose

--- root/backend/server.js
+++ root/backend/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
+const port = process.env.PORT || 3000;
 const app = express();
 const bp = require('body-parser');
-
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
-app.post('/register', (req, res) => {
-    // print the form data
-    console.log(req.body);
-}
-);
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+const registerRoute = require('./routes/register');
+
+app.use('/register', registerRoute);
+
+app.listen(port, () => {
+    console.log('Server is running on port ${port}');
 }
 );
