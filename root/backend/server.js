698a4a240655e0828db24b83e94a3298cd8817ba
Leo Dao <leotomdao@gmail.com>
2022-06-21 22:10:38 +0200

    Connect app to mongoDB

--- root/backend/server.js
+++ root/backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const port = process.env.PORT || 4000;
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const bp = require('body-parser');
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL, () => console.log("Connected!"))
 
 // ROUTE IMPORTS
 const registerRoute = require('./routes/register');
