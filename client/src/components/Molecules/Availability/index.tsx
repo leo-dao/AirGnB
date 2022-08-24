2548da1f8ad3c1cf5fd6a88e3265d893a1a080d1
Leo Dao <leotomdao@gmail.com>
2022-08-23 20:21:28 -0400

    Fixed date selection bug

--- client/src/components/Molecules/Availability/index.tsx
+++ client/src/components/Molecules/Availability/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import styled from "styled-components";
+import { CalendarOutlined } from "@ant-design/icons";
 import "react-datepicker/dist/react-datepicker.css";
 
 const StyledDatePicker = styled(DatePicker)`
@@ -31,6 +32,13 @@ const DateContainer = styled.div`
     align-items: flex-start;
     `;
 
+const Calendar = styled(CalendarOutlined)`
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+
 interface AvailabilityProps {
     startDate: Date,
     endDate: Date,
@@ -40,27 +48,67 @@ interface AvailabilityProps {
 
 const Availability = (props: AvailabilityProps) => {
 
+    const checkStartDate = (date: Date) => {
+
+        const today = new Date();
+
+        // get the date after the input date
+        const datePlusOne = new Date(date);
+        datePlusOne.setDate(datePlusOne.getDate() + 1);
+
+        // checking if start date is valid
+        if (today < date) {
+            props.setStartDate(date);
+        }
+
+        // checking if end date is valid
+        if (date > props.endDate) {
+            props.setEndDate(datePlusOne);
+        }
+        else {
+            props.setStartDate(today);
+        }
+    }
+
     const checkEndDate = (date: Date) => {
-        date.getTime() > props.startDate.getTime()
-            ? props.setEndDate(date)
-            : props.setEndDate(props.startDate)
+
+        const datePlusOne = new Date(props.startDate);
+        datePlusOne.setDate(datePlusOne.getDate() + 1);
+
+        if (date > props.startDate) {
+            props.setEndDate(date);
+        }
+
+        else {
+            props.setEndDate(datePlusOne);
+        }
     }
 
     return (
         <Container>
             <DateContainer>
                 Start Date
-                <StyledDatePicker
-                    selected={props.startDate}
-                    onChange={(date: Date) => props.setStartDate(date)}
-                />
+                <div style={{
+                    position: "relative",
+                }}>
+                    <StyledDatePicker
+                        selected={props.startDate}
+                        onChange={(date: Date) => checkStartDate(date)}
+                    />
+                    <Calendar />
+                </div>
             </DateContainer>
             <DateContainer>
                 End Date
-                <StyledDatePicker
-                    selected={props.endDate}
-                    onChange={(date: Date) => checkEndDate(date)}
-                />
+                <div style={{
+                    position: "relative",
+                }}>
+                    <StyledDatePicker
+                        selected={props.endDate}
+                        onChange={(date: Date) => checkEndDate(date)}
+                    />
+                    <Calendar />
+                </div>
             </DateContainer>
         </Container>
 
