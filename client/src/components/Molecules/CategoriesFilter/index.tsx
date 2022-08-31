e93f49d57d4f52e1c3d8b1e6f7f4f30959c2be5b
Leo Dao <leotomdao@gmail.com>
2022-08-30 20:28:05 -0400

    Option to filter by subcategories

--- client/src/components/Molecules/CategoriesFilter/index.tsx
+++ client/src/components/Molecules/CategoriesFilter/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import categoryList from "../../../utils/categories";
 
-const CategoryContainer = styled.div`
+const DefaultContainer = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
@@ -10,6 +10,14 @@ const CategoryContainer = styled.div`
     margin-bottom: 1rem;
     flex-wrap: wrap;
     flex-direction: row;
+    gap: 20px;
+`;
+
+const AllContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
     gap: 20px;
 `;
 
@@ -25,30 +33,15 @@ const Button = styled.button.attrs((props: any) => props)`
     }
     `;
 
+const MoreButton = styled.button.attrs((props: any) => props)`
+    background: none;
+    border: none;
+    border-bottom: 1px solid black;
+    color: black;
+    cursor: pointer;
+    `;
+
 const CategoriesFilter = () => {
-
-    var defaultCategories = categoryList.map((category: any) => {
-        return {
-            name: category.name,
-            active: false
-        }
-    });
-
-    const [categories, setCategories] = React.useState(defaultCategories);
-
-
-    const onClick = (e: any) => {
-        // add to params
-
-        setCategories(categories.map((category: any, index: any) => {
-
-            const id = parseInt(e.target.id);
-            if (index === id) {
-                category.active = !category.active;
-            }
-            return category;
-        }));
-    };
 
     const active = {
         'background-color': 'black',
@@ -57,30 +50,118 @@ const CategoriesFilter = () => {
 
     const inactive = {
         'background-color': 'white',
-        color: '#000'
+        color: '#353535eb'
     }
 
-    var style = categories.map((category: any) => {
-        if (category.active) {
-            return active;
-        } else {
-            return inactive;
+    var defaultCategories = categoryList.map((category: any) => {
+        return {
+            name: category.name,
+            active: false,
+        }
+    });
+
+
+    interface categoryArray {
+        name: string;
+        active: boolean;
+        parent: string;
+    };
+
+    var subCategories: categoryArray[] = [];
+
+    categoryList.map((category: any) => {
+        if (category.name !== 'Other') {
+            category.type.map((subCategory: any) => {
+                subCategories.push({
+                    name: subCategory,
+                    active: false,
+                    parent: category.name,
+                });
+            }
+            )
         }
     }
     );
 
-    return (
-        <CategoryContainer>
-            {categories.map((category, index) => {
+    const [subCategoriesState, setSubCategoriesState] = React.useState(subCategories);
+    const [defaultCategoriesState, setDefaultCategoriesState] = React.useState(defaultCategories);
+    const [complete, setComplete] = React.useState(false);
+    const [showText, setShow] = React.useState('Show more');
+
+    const flip = (e: any) => {
+        setComplete(!complete);
+        showText === 'Show more' ? setShow('Show less') : setShow('Show more');
+    }
+
+    var defaultClick = (e: any) => {
+        const id = parseInt(e.target.id);
+        setDefaultCategoriesState(defaultCategoriesState.map((category: any, index: any) => {
+            if (index === id) {
+                category.active = !category.active;
+                category.style = category.active ? active : inactive;
+            }
+            return category;
+        }));
+    };
+
+    const allClick = (e: any) => {
+        var count = -1;
+        const id = parseInt(e.target.id);
+        setSubCategoriesState(subCategoriesState.map((category: any) => {
+            count++;
+            if (count === id) {
+                category.active = !category.active;
+            }
+            return category;
+        }));
+    };
+
+    var defaultButtons = (
+
+        <DefaultContainer>
+            {defaultCategoriesState.map((category: any, index: any) => {
                 return (
                     <Button
                         id={index}
-                        onClick={onClick}
-                        style={style[index]}>
-                        {category.name}</Button>
-                );
-            })}
-        </CategoryContainer>
+                        onClick={defaultClick}
+                        style={category.active ? active : inactive}>
+                        {category.name}
+                    </Button>
+                )
+            }
+            )}
+        </DefaultContainer>
+    );
+
+    // add dummy first element?
+
+    let count = -1;
+    const allButtons = subCategoriesState.map((category: any) => {
+        count++;
+        return (
+            <div>
+                {count > 0 && category.parent !== subCategoriesState[count - 1].parent ?
+                    <div>
+                        <h5>{category.parent}</h5>
+                    </div>
+                    : null}
+                <Button
+                    id={count}
+                    onClick={allClick}
+                    style={category.active ? active : inactive}>
+                    {category.name}
+                </Button>
+            </div>
+        )
+    }
+    );
+
+
+    return (
+        <div>
+            {complete ? allButtons : defaultButtons}
+            <MoreButton onClick={flip}>{showText}</MoreButton>
+        </div>
     );
 };
 
