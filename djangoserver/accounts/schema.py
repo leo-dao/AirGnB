ec84848303358194dcf1c241e7e2b2cb362f28c2
Leo Dao <leotomdao@gmail.com>
2022-09-25 18:43:08 -0400

    Created user schema

---
+++ djangoserver/accounts/schema.py
@@ -0,0 +1,20 @@
+import graphene
+from graphql_auth.schema import UserQuery, MeQuery
+from graphql_auth import mutations
+
+class AuthMutation(graphene.ObjectType):
+    register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
+    delete_account = mutations.DeleteAccount.Field()
+    update_account = mutations.UpdateAccount.Field()
+    token_auth = mutations.ObtainJSONWebToken.Field()
+    verify_token = mutations.VerifyToken.Field()
+    refresh_token = mutations.RefreshToken.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    password_reset = mutations.PasswordReset.Field()
+    resend_activation_email = mutations.ResendActivationEmail.Field()
+
+class Query(UserQuery, MeQuery, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=AuthMutation)
