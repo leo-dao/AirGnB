8bd1bd012619a787cd5ebddf9f58b5149e98fd96
Leo Dao <leotomdao@gmail.com>
2022-10-17 17:18:09 -0400

    Simplified to remove bug

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -3,38 +3,6 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from listings.models import Listing
 import uuid
 
-class UserAccountManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
-        if not email:
-            raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have a username')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-    def create_superuser(self, email, username, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            password=password,
-            username=username,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-    
-    def delete_user(self, user):
-        user.delete()
-        return user
-
 class User(AbstractUser):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -52,23 +20,4 @@ class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'password', 'rating', 'id', 'date_joined']
-
-
-    objects = UserAccountManager()
-    
-    def __str__(self):
-        return self.email
-    
-    def get_id(self):
-        return self.id
-
-    def get_username(self):
-        return self.username
-
-    def get_rating(self):
-        return self.rating
-    
-    # Get all listings that the user has posted
-    def get_listings(self):
-            return Listing.objects.filter(leaser=self)
+    REQUIRED_FIELDS = ['username', 'password']
--- server/accounts/schema.py
+++ server/accounts/schema.py
@@ -1,16 +1,18 @@
 import graphene
+import graphql_jwt
 from graphql_auth import mutations
 from graphql_auth.schema import UserQuery, MeQuery
 
 class AuthMutation(graphene.ObjectType):
    register = mutations.Register.Field()
    verify_account = mutations.VerifyAccount.Field()
-   token_auth = mutations.ObtainJSONWebToken.Field()
    update_account = mutations.UpdateAccount.Field()
    resend_activation_email = mutations.ResendActivationEmail.Field()
    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
    password_reset = mutations.PasswordReset.Field()
    password_change = mutations.PasswordChange.Field()
+
+   token_auth = mutations.ObtainJSONWebToken.Field()
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
    pass
