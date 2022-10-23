015e6dee8968350768d2e1b4ad0208f18eb1f19d
Leo Dao <leotomdao@gmail.com>
2022-10-23 19:06:16 -0400

    removed views from core application

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import index
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    #path('', index),
     path('admin/', admin.site.urls),
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
