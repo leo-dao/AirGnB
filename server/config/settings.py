ba7f0a70e25ff3a59a53e44cf5e9bc7af4dc7e99
Leo Dao <leotomdao@gmail.com>
2022-11-07 11:51:29 -0500

    Modified JWT to include UUID instead of email

--- server/config/settings.py
+++ server/config/settings.py
@@ -69,6 +69,7 @@ AUTHENTICATION_BACKENDS = [
 
 GRAPHQL_JWT = {
     'JWT_VERIFY_EXPIRATION': True,
+    'JWT_PAYLOAD_HANDLER': 'common.utils.jwt_payload',
     'JWT_LONG_RUNNING_REFRESH_TOKEN': True,
     'JWT_EXPIRATION_DELTA': timedelta(minutes=5),
     'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
